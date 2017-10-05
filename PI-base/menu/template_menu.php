369dc336ce13d8f3f3ae9a7d28533c43d72cb00e
Eduardo Maximiano <edu.maxi.96@gmail.com>
2017-10-04 21:54:21 -0300

    Novo css

---
+++ PI-base/menu/template_menu.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="template_menu.css">
+<meta charset="UTF-8">
+</head>
+	<body>
+		<ul>
+		<li><a href="Usario">Usuario</a></li>
+		<li><a href="Categoria">Categoria</a></li>
+		<li><a href="Produto">Produto</a></li>
+		<li><a href="\vexx_dev\index.php">Sair</a></li>
+</ul>
+	
+	</body>
+</html>
+		

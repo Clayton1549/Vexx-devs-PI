4b9a6072392f3bf8be638ea2925e7750398aa46a
Clayton1549 <pereira.cleiton.oliver@gmail.com>
2017-10-25 21:58:03 -0200

    menu refeito

---
+++ menu/index.php
@@ -0,0 +1,7 @@
+<?php
+include('../db/banco.php');
+include('../auth/controle.php');
+
+
+include('template_menu.php');
+?>
---
+++ menu/template_menu.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="template_menu.css">
+<meta charset="UTF-8">
+</head>
+	<body>
+		<ul>
+		<li><a href="../user/index.php">Usuario</a></li>
+		<li><a href="../categ/index.php">Categoria</a></li>
+		<li><a href="../produto/index.php">Produto</a></li>
+		<li><a href="../?logout=1">Sair</a></li>
+</ul>
+	
+    <div id ="text">
+    <h2 id="nome">Gandalf Geek Store</h2>
+    
+    <p class="apre">
+    Bem vindo à loja virtual da Gandalf Geek!
+    </p>
+    <p class="disc">
+    Esta loja foi criada para o Projeto Integrador do  curso Sistemas para Tecnologia Para Internet do <br>Centro Universitário Senac
+    <br><br>
+    Aguarde Novidades em breve!
+    </p>
+    </div>
+    
+	</body>
+</html>
+		
